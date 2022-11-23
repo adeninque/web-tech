b2aa8b32657201c46cd9ca19abb8edd5181496ff
adeninque <adilmarsov5@gmail.com>
2022-11-23 20:19:12 +0600

    db updated

--- finalproject/homework/script.js
+++ finalproject/homework/script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     url = 'https://raw.githubusercontent.com/adeninque/web-tech/main/finalproject/homework/db.json'
+    'https://raw.githubusercontent.com/adeninque/web-tech/main/finalproject/homework/media/photos/IMG_0870.JPG'
 
     const dbPromise = fetch(url).then(response => response.json().then(obj => {
         console.log(obj);
